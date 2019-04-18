42c02877085702ff59715d3cd06d4737ad97fc11
Hugh <hcl212@users.noreply.github.com>
2019-04-17 22:42:09 -0400

    enemies shown on screen

--- main.ml
+++ main.ml
@@ -1,3 +1,26 @@
+(* TODO: list of enemies, 4 rows, going side by side then down store in list
+ * TODO: player ship lasers store in list
+ * TODO: enemy lasers shooting back
+ * TODO: if player is hit game over
+ * TODO: if enemy is hit, remove
+ * TODO: if all enemies killed, win
+ * TODO:
+     *
+     * type enemy =
+     {
+         x : int; y: int;
+         width : int; height: int;
+         time : float; 
+         surf_rect: Tsdl.Sdl.rect;
+     }
+
+        type enemyList = enemy list
+
+         // when we initialize
+         let make_ship x y w h rect =
+             x; y; w; h; surf_rect = rect
+
+         *)
 
 open Tsdl
 
@@ -6,7 +29,7 @@ module State = struct
 
   (* make initial game state *)
   let make (x, y) = 
-    ( x, y, 0.0, 0.0 )  (* x  y  velocity-x  velocity-y  *)
+    ( x, y, 0.0, 0.0 )  (* x  y  velocity-x  velocity-y *)
 
   (* push with force (fx,fy) over time dt *)
   let push (fx, fy) dt state = 
@@ -41,6 +64,25 @@ module State = struct
 
 end
 
+(* enemy dead or alive *)
+type enemyLife = Dead | Live
+
+  (* 2d array of the enemy invaders *)
+  let invaders = Array.make_matrix 4 7 true
+
+(* coordinate to render in 2d array *)
+let invader_pos (gridx,gridy) (cellwid,cellhgt) i j =
+  (gridx + i * cellwid, gridy + j * cellhgt)
+
+(* iterate through the grid and create enemies *)
+let for_matrix fn matrix =
+  Array.iteri (fun j row ->
+    Array.iteri (fun i value ->
+      fn i j value
+    ) row
+  ) matrix
+
+(* user key presses *)
 type event = 
   Left | Right | Exit
    
@@ -50,8 +92,7 @@ let rec get_event () =
   
   if Sdl.poll_event(Some e) then
     match Sdl.Event.get e Sdl.Event.typ |> Sdl.Event.enum with
-    | `Quit -> Some Exit
-    
+    | `Quit -> Some Exit 
     | `Key_down -> 
         
         let keycode = Sdl.Event.get e Sdl.Event.keyboard_keycode in
@@ -66,7 +107,6 @@ let rec get_event () =
           end
         else
           None
-
     | _ -> get_event () (* if it's an event of another type, get the next event *)
   else
     None
@@ -77,18 +117,28 @@ let draw win rend tex state =
   (* draw the background *)
   ignore (Sdl.set_render_draw_color rend 32 32 32 255);
   ignore (Sdl.render_clear rend);
-  
-  (* draw the ball *)
+
+  (* draw the enemy *)
+  let grid_coord = (0,0) in 
+  let cell_dim = (70,70) in
+  let tex_rect = Sdl.Rect.create 150 0 150 200 in
+    invaders |> for_matrix (fun i j exists ->
+        if exists then
+            let x,y = invader_pos grid_coord cell_dim i j in
+            let dst_rect = Sdl.Rect.create x y 70 70 in
+            ignore (Sdl.render_copy ~src:tex_rect ~dst:dst_rect rend tex));
+
+  (* draw the ship *)
   let (x, y, _, _) = state in
   
-  let tex_rect = Sdl.Rect.create 0 0 200 200 in
+  let tex_rect = Sdl.Rect.create 0 0 150 200 in
   let dst_rect = Sdl.Rect.create (round x - 10) (round y - 50) 70 70 in
   ignore (Sdl.render_copy ~src:tex_rect ~dst:dst_rect rend tex);
     
   Sdl.render_present rend
 
 
-let run w h win rend tex =
+let run w h win rend tex invade =
         
   let rec loop time_prev st =
       
@@ -113,7 +163,7 @@ let run w h win rend tex =
           | Some _ -> st
         in
 
-        (* if the game state should update with time, update it: *)
+        (* if the game state should update with time, update it *)
         let st3 = State.update (w, h) dt st2 in
 
         (* draw *)
@@ -123,6 +173,7 @@ let run w h win rend tex =
         loop time_cur st3
   in
 
+  (* goes to next frame by looping *)
   loop (Int32.to_int (Sdl.get_ticks())) (State.make (0.5 *. float w, 0.98 *. float h));
   
   Sdl.destroy_texture tex;
@@ -142,13 +193,16 @@ let () =
   match Sdl.init Sdl.Init.video with 
   | Error (`Msg e) -> Sdl.log "Init error: %s" e; exit 1
   | Ok () -> 
+
     ( match Sdl.create_window ~w:width ~h:height "Spaceship Destroyer" Sdl.Window.(shown + input_focus) with 
       | Error (`Msg e) -> Sdl.log "Create window error: %s" e; exit 1
       | Ok win -> 
+
         ( match Sdl.create_renderer win ~index:(-1) ~flags:Sdl.Renderer.(accelerated + presentvsync) with
           | Error (`Msg e) -> Sdl.log "Create renderer error: %s" e; exit 1
           | Ok rend ->
-            ( match Sdl.load_bmp "img/spaceship.bmp" with
+
+            ( match Sdl.load_bmp "img/sprites.bmp" with
             | Error (`Msg e)  -> Sdl.log "Load bmp error: %s" e; exit 1
             | Ok surf ->
 
@@ -173,7 +227,7 @@ let () =
                 begin match Sdl.create_texture_from_surface rend surf with
                   | Ok tex -> 
                       (* all is good, run the program *)
-                      run width height win rend tex
+                      run width height win rend tex invaders
 
                   | Error (`Msg e) -> Sdl.log "Creating texture error: %s" e; exit 1
                 end
